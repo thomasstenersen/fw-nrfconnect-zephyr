22fff8d35ce663b5ef7e427236f8920643bbde76
Pavel Vasilyev <pavel.vasilyev@nordicsemi.no>
2019-02-01 09:17:33 +0100

    Make Entropy nRF5 driver optional
    Signed-off-by: Pavel Vasilyev <pavel.vasilyev@nordicsemi.no>

--- samples/drivers/entropy/src/main.c
+++ samples/drivers/entropy/src/main.c
@@ -7,6 +7,46 @@
 #include <zephyr.h>
 #include <entropy.h>
 #include <stdio.h>
+#include <kernel.h>
+#include <irq.h>
+#include <nrf52.h>
+
+static void my_isr(void *param)
+{
+	printf("Inside interrupt\n");
+
+	struct device *dev = param;
+
+	for (size_t j = 0; j < 5; j++) {
+#define BUFFER_LENGTH 10
+		u8_t buffer[BUFFER_LENGTH] = {0};
+		int r;
+
+		/* The BUFFER_LENGTH-1 is used so the driver will not
+		 * write the last byte of the buffer. If that last
+		 * byte is not 0 on return it means the driver wrote
+		 * outside the passed buffer, and that should never
+		 * happen.
+		 */
+		r = entropy_get_entropy_isr(dev, buffer, BUFFER_LENGTH-1, ENTROPY_BUSYWAIT);
+//		if (!r) {
+//			printf("entropy_get_entropy_isr failed: %d\n", r);
+//			break;
+//		}
+
+		if (buffer[BUFFER_LENGTH-1] != 0) {
+			printf("entropy_get_entropy_isr buffer overflow\n");
+		}
+
+		for (int i = 0; i < BUFFER_LENGTH-1; i++) {
+			printf("  0x%02x", buffer[i]);
+		}
+
+		printf("\n");
+
+//		k_sleep(1000);
+	}
+}
 
 void main(void)
 {
@@ -20,8 +60,17 @@ void main(void)
 		return;
 	}
 
+	irq_connect_dynamic(QDEC_IRQn, 4 /*CONFIG_ENTROPY_NRF5_PRI*/, my_isr, dev, 0);
+	irq_enable(QDEC_IRQn);
+
+
 	printf("entropy device is %p, name is %s\n",
 	       dev, dev->config->name);
+
+	while(1)  {
+		k_sleep(1000);
+		NVIC_SetPendingIRQ(QDEC_IRQn);
+	}
 
 	while (1) {
 #define BUFFER_LENGTH 10
@@ -51,5 +100,8 @@ void main(void)
 		printf("\n");
 
 		k_sleep(1000);
+
+//		NVIC_SetPendingIRQ(QDEC_IRQn);
 	}
 }
+
